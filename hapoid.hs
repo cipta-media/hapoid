67e72ac53bed34cf3fbbe423251c4690b0ebf2db
Wisnu Adi Nurcahyo <wisnu@nurcahyo.me>
2016-11-07 14:09:10 +0700

    Finally delete the old code

--- hapoid.hs
+++ hapoid.hs
@@ -27,7 +27,8 @@ author = "Wisnu Adi Nurcahyo"
 -- Commands
 commands = ["help",  "           Display this message",
             "check", "          Check id.po file in the current directory",
-            "check <path>", "   Check file in the specified path",
+            "check <path>", "   Check file in the specified path i.e \"check my/path\"",
+            "", "               will detected as \"my/path/id.po\"",
             "about", "          Display about message"]
 
 -- Options
@@ -80,11 +81,11 @@ check path' opts' = do
 
   loc  <- getCurrentDirectory
 
-  let path = if path' == "." then "id.po" else path'
-      opts = if opts' == "" then [] else words opts'
-      file  = if path == "id.po"
+  let path  = if path' == "." then "id.po" else path'
+      opts  = if opts' == "" then [] else words opts'
+      file  = if path  == "id.po"
                 then loc ++ "/" ++ path
-                else path
+                else path ++ "/id.po"
 
   exist <- doesFileExist file
 
@@ -138,80 +139,3 @@ main = do
   if (null args)
     then display help
     else run args
-
-{-
--- Version
-version          = "0.0.1-alpha"
-versionMajor     = 0
-versionMinor     = 0
-versionRelease   = 1
-versionAttribute = "alpha"
-
--- Release Date
-released = ""
-
--- Information
-about = ["Hapoid " ++ version ++ " by Wisnu Adi Nurcahyo",
-         "Released at <not released yet>"]
-
-information = ["Usage: hapoid COMMAND [OPTIONS] [ARGS]...",
-               "",
-               "Commands:",
-               "  about                     Show Hapoid about message",
-               "  help                      Show this message and exit",
-               "  help <topic>              Show help message for the selected topic",
-               "  check <pofile>            Finding errors on .po file",
-               "  check <pofile> <options>  Finding errors on .po file with an option",
-               "",
-               "Options:",
-               "  --fuzzy                   Treat all fuzzy translation as an error"
-               ]
-
-displayInformation = do
-  putStr $ unlines information
-  exitSuccess
-
-moreInformation = "Type \"hapoid help\" for more information!"
-
-errorIn :: String -> String -> String
-errorIn typ msg' = do
-  let msg = show msg'
-
-  case typ of []    -> unlines ["Error: No such command " ++ msg ++ "!", "", moreInformation]
-              "cmd" -> unlines ["Error: " ++ msg ++ " command need one more argument!", "", moreInformation]
-              "opt" -> unlines ["Error: Option executed along with a command!", "", moreInformation]
-              _     -> unlines ["Error: Unknown error appear!", "", moreInformation]
-
--- Command Executor
-singleCmd :: [String] -> String
-singleCmd (arg:_) = do
-  case arg of "help"    -> unlines information
-              "about"   -> unlines about
-              "check"   -> errorIn "cmd" "check"
-              "--fuzzy" -> errorIn "opt" []
-              _         -> errorIn [] arg
-
--- Command Parser
-command = ["help", "check", "about"]
-
-commander :: [String] -> IO ()
-commander args = do
-  if length args == 1
-    then putStr $ singleCmd args
-    else do
-      if elem (head args) command
-        then putStrLn "Execute command!"
-        else putStr $ errorIn [] (head args)
-
--- Main Program
-main :: IO ()
-main = do
-  args <- getArgs
-
-  when (null args)
-    displayInformation
-
-  print args
-  commander args
-
--}

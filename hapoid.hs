426ecd6a01b00f3931358cee755e456535959a99
Wisnu Adi Nurcahyo <wisnu@nurcahyo.me>
2016-11-08 11:49:21 +0700

    Add fixWords

--- hapoid.hs
+++ hapoid.hs
@@ -6,28 +6,27 @@
   URL:          github.com/wisn/hapoid
 -}
 
--- Libraries
-import System.IO
-import System.Exit
-import System.Directory
-import System.Environment
-import Control.Monad
+-- [Libraries]
+import System.Directory (getCurrentDirectory, doesFileExist)
+import System.Environment (getArgs)
+import Data.Char (toLower)
+import Data.List (isInfixOf)
 
--- Version
+-- [Version]
 version          = "0.0.1-alpha"
 versionMajor     = "0"
 versionMinor     = "0"
 versionRelease   = "1"
 versionAttribute = "alpha"
 
--- Release
+-- [Release]
 released = "<not released yet>" -- > Not released yet
 
--- About
+-- [About]
 name   = "Hapoid"
 author = "Wisnu Adi Nurcahyo"
 
--- Commands
+-- [Commands]
 commands = ["help",  "           Display this message",
             "check", "          Check id.po file in the current directory",
             "check <path>", "   Check file in the specified path i.e \"check my/path\"",
@@ -35,38 +34,45 @@ commands = ["help",  "           Display this message",
             "about", "          Display about message",
             "version", "         Show Hapoid version"]
 
--- Options
+-- [Options]
 options = ["--fuzzy", "        Review all fuzzy-translations"]
 
--- Modules
+-- [Modules]
+-- Display a message
 display :: [String] -> IO ()
 display text = putStr $ unlines text
 
+-- Extract an information like `commands` or `options`
 extract :: [String] -> [String]
 extract [] = []
 extract (cmd:inf:next) = [cmd ++ inf] ++ (extract next)
 
+-- Detect and get a command
 getCmd :: String -> String
 getCmd input
   | isOption input = ""
   | otherwise      = input
 
+-- Check, it's a command?
 isOption :: String -> Bool
 isOption arg
   | (arg !! 0) == '-' && (arg !! 1) == '-' = True
   | otherwise = False
 
+-- Detect and get an option
 getOption :: [String] -> String
 getOption []     = []
 getOption (x:xs)
   | isOption x = x ++ " " ++ (getOption xs)
   | otherwise  = getOption xs
 
+-- Detect and get a path
 getPath :: String -> String
 getPath str
   | not (isOption str) = str
   | otherwise          = "."
 
+-- Parse arguments into separated list `cmd` `path` `opts`
 parse :: [String] -> [String]
 parse (arg:args) = do
   let cmd   = getCmd arg
@@ -78,54 +84,141 @@ parse (arg:args) = do
 
   return result
 
+-- Makes text green
+makesGreen :: String -> String
+makesGreen text = "\x1b[32m" ++ text ++ "\x1b[0m"
+
+-- Makes text blue
+makesBlue :: String -> String
+makesBlue text = "\x1b[34m" ++ text ++ "\x1b[0m"
+
+-- Makes text yellow
+makesYellow :: String -> String
+makesYellow text = "\x1b[33m" ++ text ++ "\x1b[0m"
+
+-- Makes text red
+makesRed :: String -> String
+makesRed text = "\x1b[31m" ++ text ++ "\x1b[0m"
+
+-- Display a success message
 showSucc :: String -> String
-showSucc msg = "[" ++ "\x1b[32m" ++ "INFO" ++ "\x1b[0m" ++ "] " ++ msg
+showSucc msg = "[" ++ (makesGreen "SUCC") ++ "] " ++ msg
 
+-- Display an information message
 showInfo :: String -> String
-showInfo msg = "[" ++ "\x1b[34m" ++ "INFO" ++ "\x1b[0m" ++ "] " ++ msg
+showInfo msg = "[" ++ (makesBlue "INFO") ++ "] " ++ msg
 
+-- Display a warning message
 showWarn :: String -> String
-showWarn msg = "[" ++ "\x1b[33m" ++ "WARN" ++ "\x1b[0m" ++ "] " ++ msg
+showWarn msg = "[" ++ (makesYellow "WARN") ++ "] " ++ msg
 
+-- Display an error message
 showErr :: String -> String
-showErr msg = "[" ++ "\x1b[31m" ++ "ERR" ++ "\x1b[0m" ++ "]  " ++ msg
+showErr msg = "[" ++ (makesRed "ERR") ++ "]  " ++ msg
 
+-- Check and truncate path information
 simplify :: String -> String
 simplify path = do
-  if (length path) > 25
-    then reverse $ (take 25 (reverse path)) ++ "..."
+  if (length path) > 40
+    then reverse $ (take 40 (reverse path)) ++ "..."
     else path
 
--- Checker
+isMsgstr :: String -> Bool
+isMsgstr text
+  | text == ""                      = False
+  | (head $ words text) == "msgstr" = True
+  | otherwise                       = False
+
+getMsgstr :: String -> String
+getMsgstr text' = do
+  let text    = words text'
+      content = unwords $ tail text
+
+  if (head text) == ""
+    then ""
+    else content
+
+reviewSuggestion :: String -> String -> String
+reviewSuggestion sugg from = "[" ++ (makesBlue "SUGG") ++ "] You may use " ++ (makesBlue sugg) ++ " instead of " ++ (makesRed from)
+
+-- [Reviewer]
+-- Review the content
+fixWords :: String -> String
+fixWords text
+  | isInfixOf "font" text        = reviewSuggestion "huruf" "font"
+  | isInfixOf "update" text      = reviewSuggestion "pembaruan" "update"
+  | isInfixOf "diinstalasi" text = reviewSuggestion "dipasang" "diinstalasi"
+  | otherwise                    = ""
+
+recursiveCheck :: [String] -> [String]
+recursiveCheck [] = []
+recursiveCheck (x:xs) = do
+  let check = fixWords (map toLower x)
+
+  if check /= ""
+    then check : (recursiveCheck xs)
+    else recursiveCheck xs
+
+review :: [String] -> Integer -> IO ()
+review [] _ = do
+  putStrLn []
+  putStrLn $ showSucc "Checking done!"
+review (x:xs) line = do
+  let f = if x == "" then "" else head (words x)
+
+  if isMsgstr f
+    then do
+      let msgstr = getMsgstr x
+          review = recursiveCheck (words msgstr)
+
+      if (length review) > 0
+        then do
+          putStrLn $ "[Line: " ++ (show line) ++ "]"
+          putStrLn $ unlines review
+        else return ()
+    else return ()
+
+  review xs (line + 1)
+
+-- Check both given path and option(s)
 check :: String -> String -> IO ()
-check path' opts' = do
+check path'' opts' = do
   putStrLn $ name ++ " " ++ version
   putStrLn []
 
   loc  <- getCurrentDirectory
 
-  let path  = if path' == "." then "id.po" else path'
+  let path'  = if path'' == "." then "id.po" else path''
       opts  = if opts' == "" then [] else words opts'
-      file  = if path  == "id.po"
-                then loc ++ "/" ++ path
-                else path ++ "/id.po"
+      path  = if path'  == "id.po"
+                then loc ++ "/" ++ path'
+                else path' ++ "/id.po"
 
-  exist <- doesFileExist file
+  exist <- doesFileExist path
 
   if exist
     then do
-      putStrLn $ showInfo ("Checking " ++ simplify file)
+      file <- readFile path
+
+      let content = lines file
+
+      putStrLn $ showInfo ("Checking " ++ simplify path)
       putStrLn []
+
+      review content 1
     else
-      putStrLn $ showErr ("File " ++ (simplify file) ++ " didn't exist!")
+      putStrLn $ showErr ("File " ++ (simplify path) ++ makesRed " didn't exist!")
 
   putStrLn []
 
--- Messages
+-- [Messages]
+-- About message
 about = ["",
          name ++ " " ++ version ++ " by " ++ author,
          "Released at " ++ released,
          ""]
+
+-- Help message
 help  = ["Usage: hapoid COMMAND [PATH] [OPTION] [ARGS]...",
          "",
          "Commands",
@@ -135,18 +228,20 @@ help  = ["Usage: hapoid COMMAND [PATH] [OPTION] [ARGS]...",
          unlines $ extract options,
          ""]
 
--- Executor
+-- [Executor]
+-- Execute given arguments
 execute :: String -> String -> String -> IO ()
 execute cmd path opts
   | cmd == "help"    = display help
   | cmd == "about"   = display about
   | cmd == "check"   = check path opts
-  | cmd == "version" = display ["", name ++ " " ++ version, ""]
+  | cmd == "version" = display [name ++ " " ++ version]
   | otherwise = display ["",
                          showErr "Unknown command!",
-                         showInfo "Please check available commands with \"hapoid help\"!",
+                         showInfo "Please check available commands with " ++ (makesBlue "hapoid help") ++ "!",
                          ""]
 
+-- Run the program
 run :: [String] -> IO ()
 run args = do
   let str   = words $ unwords $ parse args
@@ -161,7 +256,7 @@ run args = do
         execute cmd path opts
       else execute cmd path opts
 
--- Main
+-- [Main]
 main = do
   args <- getArgs
 
